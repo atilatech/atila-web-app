6b9dc3c9f2cb36b58ed33903e6e10c88c0330036
ademidun <tomiademidun@gmail.com>
2018-09-06 07:22:03 -0400

    added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
+import {
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatDialogModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatIconRegistry,
+  MatInputModule,
+  MatMenuModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatSelectModule, MatSlideToggleModule,
+  MatSnackBarModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -10,7 +26,20 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatMenuModule,
+    MatIconModule,
+    MatSnackBarModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatAutocompleteModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
