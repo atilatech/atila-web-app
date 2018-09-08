a601a150024048531f8dc2a5b400bbe5638954ce
ademidun <tomiademidun@gmail.com>
2018-09-07 21:05:02 -0400

    added serviceworker to main.ts and removed from index.html

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,20 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
+// https://github.com/angular/angular-cli/issues/9021
+// https://blog.angular-university.io/service-workers/
+// https://juristr.com/blog/2018/01/ng-app-runtime-config/
+
+if ('serviceWorker' in navigator && environment.production) {
+  console.log("Service Worker in main.ts");
+  window.addEventListener('load', () => {
+    console.log("on page Load Service Worker in main.ts");
+    navigator.serviceWorker.register('/ngsw-worker.js', {
+      scope: '/',
+    })
+      .then(registration => {
+        console.log("Service Worker registration completed main.ts", registration);
+      });
+  });
+}
